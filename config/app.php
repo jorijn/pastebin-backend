52706b4c19e4e935934596d3ebbbc9e4b0a30100
Jorijn Schrijvershof <jorijn@jorijn.com>
2017-06-24 19:49:58 +0200

    added hashids for laravel

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Paste
+         */
+        Vinkla\Hashids\HashidsServiceProvider::class
     ],
 
     /*
@@ -225,7 +229,7 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
-
+        'Hashids' => Vinkla\Hashids\Facades\Hashids::class
     ],
 
 ];
