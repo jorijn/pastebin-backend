577172e0022f156b2a7cd3064e8ef7de0a952e49
Jorijn Schrijvershof <jorijn@jorijn.com>
2017-06-25 08:29:23 +0200

    implemented getting and setting of pastes including rate limiting

--- app/Http/Controllers/PasteController.php
+++ app/Http/Controllers/PasteController.php
@@ -2,16 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Paste;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PasteController extends Controller
 {
-    public function postPaste(Request $request)
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function postPaste(Request $request): JsonResponse
     {
         $this->validate($request, [
             'language'  => 'required|exists:languages,language',
             'content'   => 'required|min:1',
             'retention' => 'numeric|min:0|max:10080',
         ]);
+
+        $paste = Paste::createFromRequest($request);
+
+        return response()->json($paste);
+    }
+
+    /**
+     * Finds a Paste by its hash and returns it. Will
+     * return 404 otherwise.
+     *
+     * @param string $hash
+     * @return JsonResponse
+     */
+    public function getPaste(string $hash): JsonResponse
+    {
+        $paste = Paste::where(['hash' => $hash])->first();
+        abort_if(is_null($paste) === true, 404, 'Paste not found');
+
+        return response()->json($paste);
     }
 }
--- app/Paste.php
+++ app/Paste.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 /**
  * App\Paste.
@@ -29,6 +31,53 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Paste extends Model
 {
-    protected $fillable = ['content'];
     protected $dates = ['updated_at', 'created_at', 'expires_at'];
+    protected $visible = [ 'content', 'hash', 'language' ];
+    protected $appends = ['language'];
+
+    /**
+     * Saves a new Paste to the database created from given request
+     * parameters. Assumes $request has been validated already.
+     *
+     * @param Request $request
+     * @return Paste
+     */
+    public static function createFromRequest(Request $request): Paste
+    {
+        $language = Language::where('language', $request->get('language'))->first();
+        abort_if(is_null($language) === true, 404, 'Language not found');
+
+        $paste = new static;
+        $paste->content = $request->get('content');
+        $paste->language_id = $language->id;
+        $paste->ip = $request->ip();
+
+        $retention = $request->get('retention');
+        if (is_numeric($retention) && $retention > 0) {
+            $paste->expires_at = Carbon::now()->addMinutes($retention);
+        }
+
+        $paste->save();
+
+        return $paste;
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function language()
+    {
+        return $this->belongsTo(Language::class);
+    }
+
+    /**
+     * Shorthand for returning the corresponding language
+     * as a string.
+     *
+     * @return mixed
+     */
+    public function getLanguageAttribute()
+    {
+        return $this->attributes['language'] = $this->language()->first()->language;
+    }
 }
